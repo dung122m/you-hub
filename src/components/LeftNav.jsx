9268146c8abe08db775e3cbec5e38327153f39c1
dungtran122m <dung.tran122@hcmut.edu.vn>
2023-02-17 15:01:37 +0700

    add video

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { categories } from "../utils/constants";
 import { Context } from "../context/contextApi";
-import { LeftNavMenuItem } from "./LeftNavMenuItem";
+import LeftNavMenuItem from "./LeftNavMenuItem";
 const LeftNav = () => {
-  return <div>LeftNav</div>;
+  const { selectedCategory, setSelectedCategory, mobileMenu } =
+    useContext(Context);
+  const navigate = useNavigate();
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case "category":
+        return setSelectedCategory(name);
+      case "home":
+        return setSelectedCategory(name);
+      case "menu":
+        return setSelectedCategory(name);
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-0 transition-all">
+      <div className="flex px-5 flex-col">
+        {categories.map((item) => {
+          return (
+            <React.Fragment key={item.name}>
+              <LeftNavMenuItem
+                text={item.type === "home" ? "Trang chủ" : item.name}
+                icon={item.icon}
+                action={() => {
+                  clickHandler(item.name, item.type);
+                  navigate("/");
+                }}
+                className={`${
+                  selectedCategory === item.name ? "bg-white/[0.15]" : ""
+                }`}
+              />
+              {item.divider && <hr className="my-5 border-white/[0.2]" />}
+            </React.Fragment>
+          );
+        })}
+        <hr className="my-5 border-white/[0.2]" />
+        <div className="text-orange-400 text-[16px] text-center">
+          Clone by Dung Tran
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+import { abbreviateNumber } from "js-abbreviation-number";
+import { Link } from "react-router-dom";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+const VideoCard = ({ video }) => {
+  return (
+    <Link to={`/video/${video.videoId}`}>
+      <div className="flex flex-col mb-8 ">
+        <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
+          <img
+            className="h-full w-full object-cover"
+            src={video?.thumbnails?.[0]?.url}
+            alt=""
+          />
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default VideoCard;
