0cd682f3f8085851aa0ab21761baf801d991ef8f
dungtran122m <dung.tran122@hcmut.edu.vn>
2023-02-20 14:54:29 +0700

    done videoscreen

--- src/shared/videoLength.jsx
+++ src/shared/videoLength.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import moment from "moment";
 
-const videoLength = () => {
-  return <div>videoLength</div>;
+const VideoLength = ({ time }) => {
+  const videoLengthInSeconds = moment()
+    .startOf("day")
+    .seconds(time)
+    .format("H:mm:ss");
+  return (
+    <div className="absolute bottom-2 right-2 bg-black text-sm px-1 text-white rounded-md">
+      {videoLengthInSeconds}
+    </div>
+  );
 };
 
-export default videoLength;
+export default VideoLength;
